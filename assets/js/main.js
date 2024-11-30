2221e22c9a1cf8a9ea2c68da0dd3e39a2f72635e
Farid Bouchdak <github@faridbouchdak.com>
2024-11-30 10:04:57 +0100

    fresh chat widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,17 @@ document.querySelectorAll('section').forEach(section => {
     section.classList.add('transition-all', 'duration-1000', 'opacity-0', 'translate-y-10');
     observer.observe(section);
 });
+
+// fresh chat widget
+var timeInMillis = 5000;
+setTimeout(function() {
+  window.fcWidgetMessengerConfig = { 
+    open: true,
+  }
+},timeInMillis);
+
+// Dynamically load the chat widget script
+const script = document.createElement('script');
+script.src = '//eu.fw-cdn.com/12945358/902902.js';
+script.setAttribute('chat', 'true');
+document.body.appendChild(script);
